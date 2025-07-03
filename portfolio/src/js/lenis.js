e37499da2831f8c11c3ae2a271cc9f534cd93399
Sergio Meneguzzo <sergio03.mene@gmail.com>
2025-07-03 17:20:42 +0200

    Init

---
+++ portfolio/src/js/lenis.js
@@ -0,0 +1,22 @@
+import Lenis from '@studio-freight/lenis'
+
+let lenis
+
+export function initLenis() {
+    lenis = new Lenis()
+
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+}
+
+export function scrollToTarget(target) {
+    lenis.scrollTo(target, {
+        offset: 0,
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+    })
+}
---
+++ portfolio/src/js/main.js
@@ -0,0 +1,8 @@
+import '../scss/main.scss'
+import {initLenis} from "./lenis.js";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "./cursor.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    initLenis();
+});
